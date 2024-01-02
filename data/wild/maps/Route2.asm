7b69cbcc552de71b1b2dde4611b3fab16b38486d
Joey Tidmore <jtidmore.math@gmail.com>
2024-01-01 23:02:58 -0600

    Update wild mons up to Viridian Forest

--- data/wild/maps/Route2.asm
+++ data/wild/maps/Route2.asm
@@ -1,23 +1,23 @@
 Route2WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  3, RATTATA
-	db  3, PIDGEY
-	db  4, PIDGEY
-	db  4, RATTATA
-	db  5, PIDGEY
-IF DEF(_RED)
-	db  3, WEEDLE
-	db  2, RATTATA
 	db  5, RATTATA
-	db  4, WEEDLE
+	db  5, PIDGEY
+	db  6, POLIWAG
+	db  6, RATTATA
+	db  7, PIDGEY
+IF DEF(_RED)
 	db  5, WEEDLE
+	db  6, PIDGEY
+	db  7, RATTATA
+	db  6, WEEDLE
+	db  7, KAKUNA
 ENDC
 IF DEF(_BLUE)
-	db  3, CATERPIE
-	db  2, RATTATA
-	db  5, RATTATA
-	db  4, CATERPIE
 	db  5, CATERPIE
+	db  6, PIDGEY
+	db  7, RATTATA
+	db  6, CATERPIE
+	db  7, METAPOD
 ENDC
 	end_grass_wildmons
 
--- data/wild/maps/Route22.asm
+++ data/wild/maps/Route22.asm
@@ -1,27 +1,27 @@
 Route22WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db  3, RATTATA
-IF DEF(_RED)
-	db  3, NIDORAN_M
-	db  4, RATTATA
-	db  4, NIDORAN_M
-	db  2, RATTATA
-	db  2, NIDORAN_M
-	db  3, SPEAROW
 	db  5, SPEAROW
-	db  3, NIDORAN_F
+IF DEF(_RED)
+	db  5, NIDORAN_M
+	db  5, RATTATA
+	db  6, NIDORAN_M
+	db  5, MANKEY
+	db  5, MANKEY
+	db  6, SPEAROW
+	db  6, MANKEY
 	db  4, NIDORAN_F
+	db  5, NIDORAN_F
 ENDC
 IF DEF(_BLUE)
-	db  3, NIDORAN_F
-	db  4, RATTATA
-	db  4, NIDORAN_F
-	db  2, RATTATA
-	db  2, NIDORAN_F
-	db  3, SPEAROW
-	db  5, SPEAROW
-	db  3, NIDORAN_M
-	db  4, NIDORAN_M
+	db  5, NIDORAN_F
+	db  5, RATTATA
+	db  6, NIDORAN_F
+	db  5, MANKEY
+	db  5, MANKEY
+	db  6, SPEAROW
+	db  6, MANKEY
+	db  5, NIDORAN_M
+	db  5, NIDORAN_M
 ENDC
 	end_grass_wildmons
 
