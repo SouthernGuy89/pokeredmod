6ea7c2be9fceefbb533af08fbb79feb754a034f2
Joey Tidmore <jtidmore.math@gmail.com>
2024-01-01 23:02:58 -0600

    Update wild mons up to Viridian Forest

--- data/wild/maps/ViridianForest.asm
+++ data/wild/maps/ViridianForest.asm
@@ -1,27 +1,26 @@
 ViridianForestWildMons:
 	def_grass_wildmons 8 ; encounter rate
 IF DEF(_RED)
-	db  4, WEEDLE
-	db  5, KAKUNA
-	db  3, WEEDLE
+	db  6, WEEDLE
+	db  7, KAKUNA
 	db  5, WEEDLE
-	db  4, KAKUNA
+	db  6, WEEDLE
 	db  6, KAKUNA
-	db  4, METAPOD
-	db  3, CATERPIE
+	db  8, KAKUNA
+	db  6, METAPOD
 ENDC
 IF DEF(_BLUE)
-	db  4, CATERPIE
-	db  5, METAPOD
-	db  3, CATERPIE
+	db  6, CATERPIE
+	db  7, METAPOD
 	db  5, CATERPIE
-	db  4, METAPOD
+	db  6, CATERPIE
 	db  6, METAPOD
-	db  4, KAKUNA
-	db  3, WEEDLE
+	db  8, METAPOD
+	db  6, KAKUNA
 ENDC
-	db  3, PIKACHU
-	db  5, PIKACHU
+	db  8, PIKACHU
+	db  9, PIKACHU
+	db 10, PIKACHU
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
