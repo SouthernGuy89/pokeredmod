52fc3efd1779fc7b2b11377a6c674db13343fb60
Joey Tidmore <jtidmore.math@gmail.com>
2024-01-06 16:38:47 -0600

    add fire type to Route 3 and buff levels

--- data/wild/maps/Route3.asm
+++ data/wild/maps/Route3.asm
@@ -1,15 +1,25 @@
 Route3WildMons:
 	def_grass_wildmons 20 ; encounter rate
-	db  6, PIDGEY
-	db  5, SPEAROW
-	db  7, PIDGEY
-	db  6, SPEAROW
-	db  7, SPEAROW
-	db  8, PIDGEY
+IF DEF(_RED)
+	db  10, GROWLITHE
+	db  10, SPEAROW
+	db  11, GROWLITHE
+	db  11, SPEAROW
+	db  12, SPEAROW
+	db  12, GROWLITHE
+ENDC
+IF DEF(_BLUE)
+	db  10, VULPIX
+	db  10, SPEAROW
+	db  11, VULPIX
+	db  11, SPEAROW
+	db  12, SPEAROW
+	db  12, VULPIX
+ENDC
 	db  8, SPEAROW
-	db  3, JIGGLYPUFF
-	db  5, JIGGLYPUFF
-	db  7, JIGGLYPUFF
+	db  10, JIGGLYPUFF
+	db  11, JIGGLYPUFF
+	db  12, JIGGLYPUFF
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
