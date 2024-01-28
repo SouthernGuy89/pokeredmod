9ebb0c6ba2aa0a34ae8e56d5c1598f08a9a895b6
Joey Tidmore <jtidmore.math@gmail.com>
2024-01-27 21:16:28 -0600

    adjust Route 4 levels

--- data/wild/maps/MtMoonB2F.asm
+++ data/wild/maps/MtMoonB2F.asm
@@ -1,15 +1,15 @@
 MtMoonB2FWildMons:
 	def_grass_wildmons 10 ; encounter rate
+	db 13, ZUBAT
+	db 13, GEODUDE
 	db 14, ZUBAT
 	db 14, GEODUDE
-	db 15, ZUBAT
-	db 15, GEODUDE
-	db 16, ZUBAT
+	db 14, ZUBAT
+	db 14, PARAS
 	db 15, PARAS
-	db 17, PARAS
+	db 14, CLEFAIRY
+	db 15, ZUBAT
 	db 15, CLEFAIRY
-	db 17, ZUBAT
-	db 17, CLEFAIRY
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
