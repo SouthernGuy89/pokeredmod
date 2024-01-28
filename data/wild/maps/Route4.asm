aa77f705a52b6cdac1d176e8c3865e31df9d9023
Joey Tidmore <jtidmore.math@gmail.com>
2024-01-27 21:16:28 -0600

    adjust Route 4 levels

--- data/wild/maps/Route4.asm
+++ data/wild/maps/Route4.asm
@@ -1,25 +1,25 @@
 Route4WildMons:
 	def_grass_wildmons 20 ; encounter rate
-	db 16, RATTATA
-	db 16, SPEAROW
-	db 15, RATTATA
+	db 14, RATTATA
+	db 14, SPEAROW
+	db 13, RATTATA
 IF DEF(_RED)
+	db 14, EKANS
+	db 14, SPEAROW
 	db 15, EKANS
+	db 15, RATTATA
 	db 15, SPEAROW
-	db 16, EKANS
-	db 17, RATTATA
-	db 17, SPEAROW
+	db 14, EKANS
 	db 15, EKANS
-	db 17, EKANS
 ENDC
 IF DEF(_BLUE)
+	db 14, SANDSHREW
+	db 14, SPEAROW
 	db 15, SANDSHREW
+	db 15, RATTATA
 	db 15, SPEAROW
-	db 16, SANDSHREW
-	db 17, RATTATA
-	db 17, SPEAROW
+	db 14, SANDSHREW
 	db 15, SANDSHREW
-	db 17, SANDSHREW
 ENDC
 	end_grass_wildmons
 
