493ee390a4da614a8c22a85cdad441991c12f7a0
Joey Tidmore <jtidmore.math@gmail.com>
2024-01-28 18:36:48 -0600

    Remove frustrating moves from learnsets. Machop family no longer has Meditate. Geodude family learns Selfdestruct later.

--- data/pokemon/base_stats/machamp.asm
+++ data/pokemon/base_stats/machamp.asm
@@ -10,7 +10,7 @@
 	INCBIN "gfx/pokemon/front/machamp.pic", 0, 1 ; sprite dimensions
 	dw MachampPicFront, MachampPicBack
 
-	db LOW_KICK, LEER, HEADBUTT, MEDITATE ; level 1 learnset
+	db LOW_KICK, LEER, HEADBUTT, NO_MOVE ; level 1 learnset
 	db GROWTH_MEDIUM_SLOW ; growth rate
 
 	; tm/hm learnset
