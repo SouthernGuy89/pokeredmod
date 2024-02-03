1caa8056318bace1615fb0e2c181921b8536554c
Joey Tidmore <jtidmore.math@gmail.com>
2024-02-03 09:41:15 -0600

    Increase Route 16 levels

--- data/wild/maps/Route16.asm
+++ data/wild/maps/Route16.asm
@@ -1,15 +1,15 @@
 Route16WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 20, PSYDUCK
-	db 22, DODUO
-	db 18, RATTATA
-	db 20, PSYDUCK
-	db 20, RATTATA
-	db 18, DODUO
-	db 22, DODUO
-	db 22, RATTATA
-	db 23, RATICATE
-	db 25, RATICATE
+	db 28, PSYDUCK
+	db 28, DODUO
+	db 29, RATTATA
+	db 29, PSYDUCK
+	db 28, RATTATA
+	db 29, DODUO
+	db 28, DODUO
+	db 29, RATTATA
+	db 29, RATICATE
+	db 30, RATICATE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
