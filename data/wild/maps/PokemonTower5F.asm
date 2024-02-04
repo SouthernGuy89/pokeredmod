bba4add9fe592c143a662092333991b39c7f2def
Joey Tidmore <jtidmore.math@gmail.com>
2024-02-03 19:06:28 -0600

    Increase levels Celadon Gym through Pokemon Tower

--- data/wild/maps/PokemonTower5F.asm
+++ data/wild/maps/PokemonTower5F.asm
@@ -1,15 +1,15 @@
 PokemonTower5FWildMons:
 	def_grass_wildmons 10 ; encounter rate
-	db 20, GASTLY
-	db 21, GASTLY
-	db 22, GASTLY
-	db 23, GASTLY
-	db 19, GASTLY
-	db 18, GASTLY
-	db 25, HAUNTER
-	db 20, CUBONE
-	db 22, CUBONE
-	db 24, GASTLY
+	db 33, GASTLY
+	db 34, GASTLY
+	db 33, GASTLY
+	db 34, GASTLY
+	db 33, GASTLY
+	db 33, HAUNTER
+	db 34, HAUNTER
+	db 33, CUBONE
+	db 34, CUBONE
+	db 35, HAUNTER
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
--- data/wild/maps/PokemonTower6F.asm
+++ data/wild/maps/PokemonTower6F.asm
@@ -1,15 +1,15 @@
 PokemonTower6FWildMons:
 	def_grass_wildmons 15 ; encounter rate
-	db 21, GASTLY
-	db 22, GASTLY
-	db 23, GASTLY
-	db 24, GASTLY
-	db 20, GASTLY
-	db 19, GASTLY
-	db 26, HAUNTER
-	db 22, CUBONE
-	db 24, CUBONE
-	db 28, HAUNTER
+	db 34, GASTLY
+	db 33, GASTLY
+	db 34, GASTLY
+	db 33, GASTLY
+	db 34, GASTLY
+	db 34, HAUNTER
+	db 33, HAUNTER
+	db 34, CUBONE
+	db 33, CUBONE
+	db 35, GENGAR
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
