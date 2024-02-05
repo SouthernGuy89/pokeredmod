ff39c3c05478d5971f041ccf7ee3a2ab36e2ab9b
Joey Tidmore <jtidmore.math@gmail.com>
2024-02-04 19:26:19 -0600

    Increase Seafoam Islands levels

--- data/wild/maps/SeafoamIslandsB1F.asm
+++ data/wild/maps/SeafoamIslandsB1F.asm
@@ -1,28 +1,28 @@
 SeafoamIslandsB1FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 IF DEF(_RED)
-	db 30, STARYU
-	db 30, HORSEA
-	db 32, SHELLDER
-	db 32, HORSEA
-	db 28, SLOWPOKE
-	db 30, SEEL
-	db 30, SLOWPOKE
-	db 28, SEEL
-	db 38, DEWGONG
-	db 37, SEADRA
+	db 49, STARYU
+	db 49, HORSEA
+	db 50, SHELLDER
+	db 50, HORSEA
+	db 48, SLOWPOKE
+	db 50, SEEL
+	db 50, SLOWPOKE
+	db 48, SEEL
+	db 51, DEWGONG
+	db 51, SEADRA
 ENDC
 IF DEF(_BLUE)
-	db 30, SHELLDER
-	db 30, KRABBY
-	db 32, STARYU
-	db 32, KRABBY
-	db 28, PSYDUCK
-	db 30, SEEL
-	db 30, PSYDUCK
-	db 28, SEEL
-	db 38, DEWGONG
-	db 37, KINGLER
+	db 49, SHELLDER
+	db 49, KRABBY
+	db 50, STARYU
+	db 50, KRABBY
+	db 48, PSYDUCK
+	db 50, SEEL
+	db 50, PSYDUCK
+	db 48, SEEL
+	db 51, DEWGONG
+	db 51, KINGLER
 ENDC
 	end_grass_wildmons
 
--- data/wild/maps/SeafoamIslandsB4F.asm
+++ data/wild/maps/SeafoamIslandsB4F.asm
@@ -1,28 +1,28 @@
 SeafoamIslandsB4FWildMons:
 	def_grass_wildmons 10 ; encounter rate
 IF DEF(_RED)
-	db 31, HORSEA
-	db 31, SHELLDER
-	db 33, JYNX
-	db 33, SHELLDER
-	db 29, SLOWPOKE
-	db 31, SEEL
-	db 31, SLOWPOKE
-	db 29, SEEL
-	db 39, SLOWBRO
+	db 50, HORSEA
+	db 50, SHELLDER
+	db 51, JYNX
+	db 51, SHELLDER
+	db 48, SLOWPOKE
+	db 50, SEEL
+	db 50, SLOWPOKE
+	db 48, SEEL
+	db 51, SLOWBRO
 ENDC
 IF DEF(_BLUE)
-	db 31, KRABBY
-	db 31, STARYU
-	db 33, JYNX
-	db 33, STARYU
-	db 29, PSYDUCK
-	db 31, SEEL
-	db 31, PSYDUCK
-	db 29, SEEL
-	db 39, GOLDUCK
+	db 50, KRABBY
+	db 50, STARYU
+	db 51, JYNX
+	db 51, STARYU
+	db 48, PSYDUCK
+	db 50, SEEL
+	db 50, PSYDUCK
+	db 48, SEEL
+	db 51, GOLDUCK
 ENDC
-	db 32, SQUIRTLE
+	db 18, SQUIRTLE
 	end_grass_wildmons
 
 	def_water_wildmons 0 ; encounter rate
