55240acdb5dd2bcde3cbde1105e87db4c70838c9
Joey Tidmore <jtidmore.math@gmail.com>
2024-02-06 20:25:08 -0600

    Use original Missingno TM list

--- data/pokemon/base_stats/missingno.asm
+++ data/pokemon/base_stats/missingno.asm
@@ -14,13 +14,11 @@
 	db 26 ; growth rate
 
 	; tm/hm learnset
-	tmhm MEGA_PUNCH,   MEGA_KICK,    TOXIC,        BODY_SLAM,    TAKE_DOWN,    \
-	     DOUBLE_EDGE,  BUBBLEBEAM,   WATER_GUN,    ICE_BEAM,     BLIZZARD,     \
-	     HYPER_BEAM,   SUBMISSION,   COUNTER,      SEISMIC_TOSS, RAGE,         \
-	     SOLARBEAM,    THUNDERBOLT,  THUNDER,      PSYCHIC_M,    TELEPORT,     \
-	     MIMIC,        DOUBLE_TEAM,  REFLECT,      BIDE,         METRONOME,    \
-	     SELFDESTRUCT, FIRE_BLAST,   SKULL_BASH,   REST,         THUNDER_WAVE, \
-	     PSYWAVE,      TRI_ATTACK,   SUBSTITUTE,   STRENGTH,     FLASH
+	tmhm MEGA_PUNCH,   RAZOR_WIND,   SWORDS_DANCE, MEGA_KICK,    TOXIC, \
+	     TAKE_DOWN,    DOUBLE_EDGE,  BUBBLEBEAM,   ICE_BEAM,     BLIZZARD, \
+	     SUBMISSION,   SEISMIC_TOSS, RAGE,         THUNDER,      EARTHQUAKE, \
+         FISSURE,      PSYCHIC_M,    TELEPORT,     SKY_ATTACK,   REST, \
+         THUNDER_WAVE, TRI_ATTACK,   SUBSTITUTE,   CUT,          FLY
 	; end
 
 	db 0 ; padding
