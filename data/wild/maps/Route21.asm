0d7b92a13dc5a05805039a887ed63cc93d002631
Joey Tidmore <jtidmore.math@gmail.com>
2024-02-07 20:58:37 -0600

    Increae Route 21 and Viridian Gym levels

--- data/wild/maps/Route21.asm
+++ data/wild/maps/Route21.asm
@@ -1,15 +1,15 @@
 Route21WildMons:
 	def_grass_wildmons 25 ; encounter rate
-	db 21, RATTATA
-	db 23, PIDGEY
-	db 30, RATICATE
-	db 23, FARFETCHD
-	db 21, PIDGEY
-	db 30, PIDGEOTTO
-	db 32, BULBASAUR
-	db 28, TANGELA
-	db 30, TANGELA
-	db 32, TANGELA
+	db 52, RATICATE
+	db 53, PIDGEOT
+	db 53, RATICATE
+	db 43, FARFETCHD
+	db 52, PIDGEOT
+	db 54, PIDGEOT
+	db  5, BULBASAUR
+	db 52, TANGELA
+	db 53, TANGELA
+	db 54, TANGELA
 	end_grass_wildmons
 
 	def_water_wildmons 5 ; encounter rate
@@ -19,7 +19,7 @@ Route21WildMons:
 	db  5, TENTACOOL
 	db 10, TENTACOOL
 	db 15, TENTACOOL
-	db 20, TENTACOOL
+	db 50, TENTACRUEL
 	db 30, DRATINI
 	db 35, DRAGONAIR
 	db 40, DRAGONAIR
