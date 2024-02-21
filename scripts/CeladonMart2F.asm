d8ab83221fc895e6b7ed4cb3423d263df13caaa2
Joey Tidmore <jtidmore.math@gmail.com>
2024-02-20 21:55:21 -0600

    Remove Celadon TM Mart

--- scripts/CeladonMart2F.asm
+++ scripts/CeladonMart2F.asm
@@ -4,7 +4,6 @@ CeladonMart2F_Script:
 CeladonMart2F_TextPointers:
 	def_text_pointers
 	dw_const CeladonMart2FClerk1Text,           TEXT_CELADONMART2F_CLERK1
-	dw_const CeladonMart2FClerk2Text,           TEXT_CELADONMART2F_CLERK2
 	dw_const CeladonMart2FMiddleAgedManText,    TEXT_CELADONMART2F_MIDDLE_AGED_MAN
 	dw_const CeladonMart2FGirlText,             TEXT_CELADONMART2F_GIRL
 	dw_const CeladonMart2FCurrentFloorSignText, TEXT_CELADONMART2F_CURRENT_FLOOR_SIGN
